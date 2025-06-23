873864c270423e16f9d076657e5d1765856b392f
Viktor Zarnytsky <zarnytskyy05@gmail.com>
2025-06-23 14:14:39 +0200

    hw8

---
+++ React_hw8/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListItems from './ListItems';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Performance Test</h1>
+      <ListItems />
+    </div>
+  );
+}
+
+export default App;
---
+++ React_hw8/src/ListItems.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+const ListItems = () => {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addItem = () => {
+    if (inputValue.trim() === '') return;
+    setItems((prevItems) => [...prevItems, inputValue]);
+    setInputValue('');
+  };
+
+  // Намеренная избыточная перерисовка
+  useEffect(() => {
+    console.log('Компонент ListItems обновлен');
+  // }, [items]);
+   },[]);
+
+  return (
+    <div>
+      <h2>Список элементов</h2>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addItem}>Добавить</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={`${item}-${index}`}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListItems;
